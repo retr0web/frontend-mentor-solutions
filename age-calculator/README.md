2623e4466f07465823729ea7db20385563e36092
retr0web <45012672+retr0web@users.noreply.github.com>
2024-07-25 14:09:01 +0300

    Final version

---
+++ age-calculator/README.md
@@ -0,0 +1,56 @@
+# Frontend Mentor - Age calculator app solution
+
+This is a solution to the [Age calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/age-calculator-app-dF9DFFpj-Q). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View an age in years, months, and days after submitting a valid date through the form
+- Receive validation errors if:
+  - Any field is empty when the form is submitted
+  - The day number is not between 1-31
+  - The month number is not between 1-12
+  - The year is in the future
+  - The date is invalid e.g. 31/04/1991 (there are 30 days in April)
+- View the optimal layout for the interface depending on their device's screen size
+- See hover and focus states for all interactive elements on the page
+
+### Screenshot
+
+![](./images/screenshot.png)
+
+### Links
+
+- Solution URL: [Github](https://github.com/retr0web/frontend-mentor-solutions/tree/main/age-calculator)
+- Live Site URL: [Github Pages](https://retr0web.github.io/frontend-mentor-solutions/age-calculator/)
+
+## My process
+
+### Built with
+
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- [Luxon](https://moment.github.io/luxon/) - wrapper for JavaScript dates and times
+
+### What I learned
+
+Working with the Luxon library and its documentation.
+
+## Author
+
+- Frontend Mentor - [@retr0web](https://www.frontendmentor.io/profile/retr0web)
--- age-calculator/index.js
+++ age-calculator/index.js
@@ -1,9 +1,101 @@
+import { DateTime } from "./luxon.js";
+
 const calculateButton = document.querySelector('#calculate');
 
+const yourYears = document.querySelector('#your-years');
+const yourMonths = document.querySelector('#your-months');
+const yourDays = document.querySelector('#your-days');
+
+const yearLabel = document.querySelector('#year-label');
+const monthLabel = document.querySelector('#month-label');
+const dayLabel = document.querySelector('#day-label');
+
 function calculateAge() {
-    let day = document.querySelector('#day').value;
-    let month = document.querySelector('#month').value;
-    let year = document.querySelector('#year').value;
+    let day = document.querySelector('#day');
+    let month = document.querySelector('#month');
+    let year = document.querySelector('#year');
+
+    const today = DateTime.now();
+    
+    const inputs = [day, month, year];
+
+    function clearOutput() {
+        yourYears.innerHTML = `--`;
+        yourMonths.innerHTML = `--`;
+        yourDays.innerHTML = `--`;
+    }
+    
+    for (let input of inputs) {
+        if (input.value === '') {
+            input.nextElementSibling.dataset.error = "This field is required";
+            input.nextElementSibling.innerHTML = input.nextElementSibling.dataset.error;
+            input.parentElement.classList.add('error');
+        } else if (input.id === 'day') {
+            if (input.value > 31 || input.value < 1) {
+                input.nextElementSibling.dataset.error = "Must be a valid day";
+                input.nextElementSibling.innerHTML = input.nextElementSibling.dataset.error;
+                input.parentElement.classList.add('error');
+            } else {
+                input.parentElement.classList.remove('error');
+            }
+        } else if (input.id === 'month') {
+            if (input.value > 12 || input.value < 1) {
+                input.nextElementSibling.dataset.error = "Must be a valid month";
+                input.nextElementSibling.innerHTML = input.nextElementSibling.dataset.error;
+                input.parentElement.classList.add('error');
+            } else {
+                input.parentElement.classList.remove('error');
+            }
+        } else if (input.id === 'year') {
+            if (input.value > today.c.year) {
+                input.nextElementSibling.dataset.error = "Must be in the past";
+                input.nextElementSibling.innerHTML = input.nextElementSibling.dataset.error;
+                input.parentElement.classList.add('error');
+            } else {
+                input.parentElement.classList.remove('error');
+            }
+        } else {
+            input.parentElement.classList.remove('error');
+        }
+    }
+     
+    for (let input of inputs) {
+        if (input.parentElement.classList.contains('error')) {
+            clearOutput();
+            return;
+        }
+    }
+
+    const dateOfBirth = DateTime.fromObject({day: Number(day.value), month: Number(month.value), year: Number(year.value)});
+
+    if(!dateOfBirth.isValid) {
+        day.nextElementSibling.dataset.error = "Must be a valid date";
+        day.nextElementSibling.innerHTML = day.nextElementSibling.dataset.error;
+        day.parentElement.classList.add('error');
+        clearOutput();
+        return;
+    }
+    
+    const result = today.diff(dateOfBirth, ['years', 'months', 'days']).toObject();
+
+    const 
+    {   days: calculatedDays,
+        months: calculatedMonths,
+        years: calculatedYears 
+    } = result;
+
+    yourYears.innerHTML = `${calculatedYears}`;
+    yourMonths.innerHTML = `${calculatedMonths}`;
+    yourDays.innerHTML = `${Math.floor(calculatedDays)}`;
+
+    yearLabel.innerHTML = `${calculatedYears > 1 ? 'years' : 'year'}`;
+    monthLabel.innerHTML = `${calculatedMonths > 1 ? 'months' : 'month'}`;
+    dayLabel.innerHTML = `${calculatedDays > 1 ? 'days' : 'day'}`;
+}
+
+function handleEnter(e) {
+    if (e.key === 'Enter') calculateAge();
 }
 
 calculateButton.addEventListener('click', calculateAge);
+window.addEventListener('keydown', handleEnter);
